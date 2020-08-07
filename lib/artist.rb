9b488188f998d4b2ce7d5848edd5fe746e875da8
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-07 23:59:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,26 @@
-require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
+
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
 
   @@artists = []
+  
+  def self.all
+    @@artists
+  end
 
   def initialize
     super
     @songs = []
   end
 
-  def self.all
-    @@artists
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
