fd24cd59dd16125e55bbcd49a8a88d567a134f9f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-08 00:00:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,24 @@
 
 
 class Song
+
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def artist=(artist)
-    @artist = artist
-  end
-
   def self.all
     @@songs
   end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+  
 end
